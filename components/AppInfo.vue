4d4087a40f3ec3b5a79b65b1f3536d136f0f72a7
Perez Christopher <christopher.perez1@sanitas.com>
2026-01-09 15:34:08 +0100

    chore: Test release v1.11.1 to verify PWA update prompt

--- components/AppInfo.vue
+++ components/AppInfo.vue
@@ -193,7 +193,7 @@
 const isOpen = ref(false)
 
 // App Version - also exported for use in other components
-const appVersion = '1.11.0'
+const appVersion = '1.11.1'
 
 // Tabs
 const tabs = [
@@ -258,6 +258,13 @@ const shortcuts = [
 
 // Changelog
 const changelog = [
+  {
+    version: '1.11.1',
+    date: 'January 9, 2026',
+    changes: [
+      { type: 'improvement', text: 'Test release to verify PWA update prompt functionality' }
+    ]
+  },
   {
     version: '1.11.0',
     date: 'January 9, 2026',
